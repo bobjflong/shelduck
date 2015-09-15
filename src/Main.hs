70835830c168bf007564ea308ba7108df670db87
Bob Long <robertjflong@gmail.com>
2015-09-15 12:16:10 +0100

    Give scalpel an executable

---
+++ src/Main.hs
@@ -0,0 +1,16 @@
+module Main (
+  main
+)  where
+
+import           IntercomDefinitions
+import           System.Environment
+
+printUsage :: IO ()
+printUsage = mapM_ putStrLn usageLines
+  where usageLines = ["USAGE: scalpel <definition_type>", "Current definition_types: intercom"]
+
+main = do
+  args <- getArgs
+  case args of
+    ("intercom":_) -> runIntercomDefinitions
+    _ -> printUsage
