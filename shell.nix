300df20f4350e4c214ebd82726df624ea744959c
Bob Long <robertjflong@gmail.com>
2015-09-15 14:34:28 +0100

    bump version

--- shell.nix
+++ shell.nix
@@ -10,7 +10,7 @@ let
       }:
       mkDerivation {
         pname = "scalp-webhooks";
-        version = "0.1.1.0";
+        version = "0.1.2.0";
         src = ./.;
         isLibrary = true;
         isExecutable = true;
